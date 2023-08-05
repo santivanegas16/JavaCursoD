18769aaf7a136385f9e3c4f029b3e1a65a675979
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-08-04 19:05:02 -0500

    DB finished and connected

--- Tarea_endpoints/src/main/java/com/triplebyte/controllers/EndPoints.java
+++ Tarea_endpoints/src/main/java/com/triplebyte/controllers/EndPoints.java
@@ -1,9 +1,11 @@
 package com.triplebyte.controllers;
 import com.triplebyte.models.Distribution;
 import com.triplebyte.models.Number;
+import com.triplebyte.models.Response;
 import com.triplebyte.models.Stats;
 import com.triplebyte.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,7 +14,8 @@ import java.util.List;
 @RequestMapping("/api")
 public class EndPoints {
 
-    int[] datos = {9,5,4,3,2,1,1000,6,7,7,9,999};
+    @Autowired
+    private NumbersService numbersService;
 
     @Autowired
     private Ordering order;
@@ -23,29 +26,75 @@ public class EndPoints {
     @Autowired
     private Hypergeometric hypergeometric;
 
-    @Autowired
-    private NumbersService numbersService;
-
     @GetMapping("/order")
-    public int[] order(){ return order.ordenarDatos(datos); }
+    public List<Number> order(){ return order.ordenarDatos(); }
 
     @GetMapping("/stats")
-    public Stats stats() {
-        return new Stats(order(), calcStats.mean(datos), calcStats.median(datos), calcStats.mode(datos));
+    public ResponseEntity<Response> stats() {
+        Response response = new Response();
+        try {
+            response.setResponse(new Stats(order(), calcStats.mean(), calcStats.median(), calcStats.mode()));
+            response.setSuccess(true);
+            response.setMessage("Stats calculated");
+            return ResponseEntity.status(200).body(response);
+        } catch (Exception e) {
+            response.setResponse(null);
+            response.setSuccess(false);
+            response.setMessage("Not calculated");
+            return ResponseEntity.status(400).body(response);
+        }
     }
 
     @PostMapping("/distribution")
-    public double distribution(@RequestBody Distribution distribution){
-        int tam_total_poblacion = distribution.getTam_total_poblacion();
-        int tam_muestra = distribution.getTam_muestra();
-        int tam_subconjunto_poblacion = distribution.getTam_subconjunto_poblacion();
-        int elementos_subconjunto_interes = distribution.getElementos_subconjunto_interes();
-        return hypergeometric.calculate(tam_total_poblacion, tam_muestra, tam_subconjunto_poblacion, elementos_subconjunto_interes);
+    public ResponseEntity<Response> distribution(@RequestBody Distribution distribution){
+
+        Response response = new Response();
+        try {
+            int tam_total_poblacion = distribution.getTam_total_poblacion();
+            int tam_muestra = distribution.getTam_muestra();
+            int tam_subconjunto_poblacion = distribution.getTam_subconjunto_poblacion();
+            int elementos_subconjunto_interes = distribution.getElementos_subconjunto_interes();
+            response.setResponse(hypergeometric.calculate(tam_total_poblacion, tam_muestra, tam_subconjunto_poblacion, elementos_subconjunto_interes));
+            response.setSuccess(true);
+            response.setMessage("Distribution calculated");
+            return ResponseEntity.status(200).body(response);
+        } catch (Exception e) {
+            response.setResponse(null);
+            response.setSuccess(false);
+            response.setMessage("Not calculated");
+            return ResponseEntity.status(400).body(response);
+        }
     }
 
     @GetMapping("/allNumbers")
-    public List<Number> getAllNumbers(){
-        return numbersService.getAllNumbers();
+    public ResponseEntity<Response> getAllNumbers() {
+        Response response = new Response();
+        try {
+            response.setResponse(numbersService.getAllNumbers());
+            response.setSuccess(true);
+            response.setMessage("Found");
+            return ResponseEntity.status(200).body(response);
+        } catch (Exception e) {
+            response.setResponse(null);
+            response.setSuccess(false);
+            response.setMessage("Not Found");
+            return ResponseEntity.status(400).body(response);
+        }
     }
 
+    @PostMapping("/insertNumbers")
+    public ResponseEntity<Response> createNumber(@RequestBody Number number) {
+        Response response = new Response();
+        try {
+            response.setResponse(numbersService.create(number));
+            response.setSuccess(true);
+            response.setMessage("Number Inserted");
+            return ResponseEntity.status(200).body(response);
+        } catch (Exception e) {
+            response.setResponse(null);
+            response.setSuccess(false);
+            response.setMessage("Not inserted");
+            return ResponseEntity.status(400).body(response);
+        }
+    }
 }
--- Tarea_endpoints/src/main/java/com/triplebyte/repository/NumberRepository.java
+++ Tarea_endpoints/src/main/java/com/triplebyte/repository/NumberRepository.java
@@ -1,16 +1,8 @@
 package com.triplebyte.repository;
 
-
-import com.triplebyte.models.Number;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.triplebyte.models.Number;
 
-/*
-* Long: representa el tipo de dato con relacion al id de la entidad Number
-* ES DECIR ES EL SIGUIENTE CODIGO CON RELACION AL ID DE LA CLASE Number:
-* @Id
-* @GeneratedValue(strategy = GenerationType.IDENTITY)
-* private long id;
-* */
 public interface NumberRepository extends JpaRepository<Number, Long> {
 
 }
